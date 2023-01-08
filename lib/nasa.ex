46895ff65b417f300fc1cc515bbfdf168979be26
Konstantinos Maglaras <konst.maglaras@gmail.com>
2023-01-08 18:54:17 +0200

    complete TT

--- lib/nasa.ex
+++ lib/nasa.ex
@@ -1,20 +1,28 @@
 defmodule Nasa do
-  def launch_fuel(mass, gravity) do
+  def trip(weight, destinations) do
+    destinations
+    |> Enum.reverse()
+    |> Enum.reduce(0, fn {action, gravity}, acc ->
+      additional_fuel(weight + acc, gravity, action) + acc
+    end)
+  end
+
+  defp fuel_calculation(mass, gravity, :launch) do
     trunc(mass * gravity * 0.042 - 33)
   end
 
-  def land_fuel(mass, gravity) do
+  defp fuel_calculation(mass, gravity, :land) do
     trunc(mass * gravity * 0.033 - 42)
   end
 
-  def additional_landing_fuel(mass, gravity, added_fuel \\ 0)
+  defp additional_fuel(mass, gravity, action, added_fuel \\ 0)
 
-  def additional_landing_fuel(mass, gravity, added_fuel) when mass > 0 do
-    fuel = land_fuel(mass, gravity)
-    additional_landing_fuel(fuel, gravity, added_fuel + fuel)
+  defp additional_fuel(mass, gravity, action, added_fuel) when mass > 0 do
+    fuel = fuel_calculation(mass, gravity, action)
+    additional_fuel(fuel, gravity, action, added_fuel + fuel)
   end
 
-  def additional_landing_fuel(mass, _gravity, added_fuel) do
+  defp additional_fuel(mass, _gravity, _action, added_fuel) do
     added_fuel - mass
   end
 end
--- test/nasa_test.exs
+++ test/nasa_test.exs
@@ -1,11 +1,38 @@
 defmodule NasaTest do
   use ExUnit.Case
 
-  test "calculates landing on Earth" do
-    assert Nasa.land_fuel(28801, 9.807) == 9278
-  end
+  describe "trip/2" do
+    test "calculates single landing trip to earth" do
+      assert Nasa.trip(28801, [{:land, 9.807}]) == 13447
+    end
 
-  test "calculates additional landing fuel for Earth landing" do
-    assert Nasa.additional_landing_fuel(28801, 9.807, 0) == 13447
+    test "calculates Apollo 11 trip" do
+      equipment_weight = 28801
+      trip_steps = [{:launch, 9.807}, {:land, 1.62}, {:launch, 1.62}, {:land, 9.807}]
+
+      assert Nasa.trip(equipment_weight, trip_steps) == 51898
+    end
+
+    test "calculates Mission on Mars" do
+      equipment_weight = 14606
+      trip_steps = [{:launch, 9.807}, {:land, 3.711}, {:launch, 3.711}, {:land, 9.807}]
+
+      assert Nasa.trip(equipment_weight, trip_steps) == 33388
+    end
+
+    test "calculates Passenger ship " do
+      equipment_weight = 75432
+
+      trip_steps = [
+        {:launch, 9.807},
+        {:land, 1.62},
+        {:launch, 1.62},
+        {:land, 3.711},
+        {:launch, 3.711},
+        {:land, 9.807}
+      ]
+
+      assert Nasa.trip(equipment_weight, trip_steps) == 212_161
+    end
   end
 end
