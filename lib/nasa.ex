50fd4f51f659eee3b9f3f809b597f28fb35e030a
Konstantinos Maglaras <konst.maglaras@gmail.com>
2023-01-08 19:10:03 +0200

    refactoring

--- lib/nasa.ex
+++ lib/nasa.ex
@@ -3,25 +3,24 @@ defmodule Nasa do
     destinations
     |> Enum.reverse()
     |> Enum.reduce(0, fn {action, gravity}, acc ->
-      additional_fuel(weight + acc, gravity, action) + acc
+      additional_fuel(weight + acc, gravity, action, 0) + acc
     end)
   end
 
-  defp fuel_calculation(mass, gravity, :launch) do
-    trunc(mass * gravity * 0.042 - 33)
-  end
+  defp fuel_calculation(mass, gravity, :launch), do: mass * gravity * 0.042 - 33
 
-  defp fuel_calculation(mass, gravity, :land) do
-    trunc(mass * gravity * 0.033 - 42)
-  end
-
-  defp additional_fuel(mass, gravity, action, added_fuel \\ 0)
+  defp fuel_calculation(mass, gravity, :land), do: mass * gravity * 0.033 - 42
 
   defp additional_fuel(mass, gravity, action, added_fuel) when mass > 0 do
-    fuel = fuel_calculation(mass, gravity, action)
+    fuel =
+      mass
+      |> fuel_calculation(gravity, action)
+      |> trunc()
+
     additional_fuel(fuel, gravity, action, added_fuel + fuel)
   end
 
+  # Removes mass from last iteration when fuel needed become negative
   defp additional_fuel(mass, _gravity, _action, added_fuel) do
     added_fuel - mass
   end
